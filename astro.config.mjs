fd1e6f233167bcbb694ef0a30d7782c7af3f6451
joshhovis <joshuahovis3@gmail.com>
2023-04-07 21:04:14 -0500

    Add Netlify adapter to project to enable SSR in project

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,10 @@
 import { defineConfig } from "astro/config";
+// import mdx from "@astrojs/mdx";
 
-import mdx from "@astrojs/mdx";
+import netlify from "@astrojs/netlify/functions";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  output: "server",
+  adapter: netlify(),
+});
